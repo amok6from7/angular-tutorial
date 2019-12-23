99c9ca8d8a86b5a0ac5928a55497ab4f34aa7b22
amok6from7 <kwifi7@gmail.com>
2019-12-23 23:05:01 +0900

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,43 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { MemberDetailComponent } from './member-detail.component';
+import { MembersComponent } from './members.component';
+import { MemberService } from './member.service';
+import { DashboardComponent} from './dashboard.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'members',
+        component: MembersComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MembersComponent,
+    DashboardComponent
   ],
   bootstrap:    [
     AppComponent
-  ]
+  ],
+  providers: [ MemberService ]
 })
 export class AppModule { }
